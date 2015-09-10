69ac0f925b31b91488b782cabd1ee3dadf093bb0
Jack Connor <jack.connor83@gmail.com>
2015-09-09 23:44:00 -0700

    switching to a data modelling for th efrontend branch

--- public/js/controllers/listCtrl.js
+++ public/js/controllers/listCtrl.js
@@ -3,11 +3,30 @@ angular.module('listCtrl', [])
 
 function listController($http, $routeParams){
   var self = this;
-  console.log($routeParams.id);
-  // console.log(route.curretn.params);
+  //finding the date on load and feeding it to the tmsapi to initially populate the list based on the user's location
+  var currentDate = new Date();
+  var year = currentDate.getFullYear();
+  var month = function(){
+    if(currentDate.getMonth() < 10){
+      var fullMonth = "0" + (currentDate.getMonth()+1);
+      var fullMonthInt = parseInt(fullMonth);
+      return fullMonth
+    } else {
+      return currentDate.getMonth()
+    };
+  }
+  var day = currentDate.getDate();
+  var formatDate = year + "-"+month()+"-"+day;
+  self.formatDate = formatDate;
+  console.log(formatDate);
+  //end finding today's date with formatting for api call
 
-  $http.get('http://data.tmsapi.com/v1.1/movies/showings?startDate=2015-09-09&zip=90036&api_key=qf6mzc3fkprbntfd95db3hkk')
+  //begin api call for the data to populate the list view
+  var url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate='+formatDate+'&zip=90036&api_key=qf6mzc3fkprbntfd95db3hkk'
+  console.log(url);
+  $http.get(url)
    .success(function(data){
+     console.log(data);
      self.rawData = data;
      var filteredData = [];
      var idCount = 1;
@@ -22,13 +41,13 @@ function listController($http, $routeParams){
           movieName: self.rawData[i].title, time: time,
           theatreId: showtimes[j].theatre.id,
           theatreName: showtimes[j].theatre.name,
-          id: idCount
+          id: idCount,
+          runTime: self.rawData[i].runTime
         }
         filteredData.push(item);
         idCount++;
       }
      }
-     console.log(filteredData);
      self.data = filteredData;
    })
 
@@ -36,9 +55,9 @@ function listController($http, $routeParams){
 
   var buttonCounter = true;
 
+//this is the function to open the secret button window when you click on an movie
   self.openButtons = function(movie){
     //jquery stuff
-
     if (buttonCounter) {
       // $('#'+id).css('margin-bottom', 100+"px");
       $('#'+movie.id).append('<div class="row"><div class="buttonContainer col-md-8 col-md-offset-2 col-xs-12 col-xs-offset-0" id=abc'+movie.id+'><button class="mapButton btn-default">Get Directions</button><button class="detailsButton btn-default">See Movie Details</button></div></div>')
@@ -70,6 +89,7 @@ function listController($http, $routeParams){
       console.log('something weird happened');
     }
   }
+  //end function for button window
 
   self.getMovie = function(){
     console.log('movie bitches!!!!!!!');
@@ -77,10 +97,20 @@ function listController($http, $routeParams){
     var key = "a9e6b3f7506ddf2d1499a135372be9f7";
     var url = "http://api.themoviedb.org/3/search/movie?query="+$routeParams.id+"&api_key="
 
+    //single movie detail http call to return additional movie details
     $http.get(url+key)
       .success(function(data){
       console.log(data);
+      //begin passing my data to my angular frontend via self.'s
       self.posterLink = "http://image.tmdb.org/t/p/w500"+data.results[0].poster_path;
+      self.movieDescription = data.results[0].overview;
+      console.log(self.movieDescription);
+      self.movieTitle = data.results[0].title;
+      console.log(self.movieTitle);
+      self.runtime = function(){
+        console.log(self.data);
+      }
+
     })
   }
 }
